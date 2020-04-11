c4d0e260439630b868f02f706847d9749c2bd07a
KamelZyadeh <kamelzyadeh94@gmail.com>
2020-04-11 19:25:06 +0300

    adding an airplane

---
+++ Airplane.java
@@ -0,0 +1,12 @@
+package kamel;
+
+public class Airplane {
+    public int id;
+    public String name;
+    public int capacity;
+
+    public Airplane (int id, String name , int capacity){
+        this.id=id;
+        this.capacity=capacity;
+        this.name=name;    }
+}
