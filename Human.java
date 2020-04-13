9f846bc6b10d3a3264125ba023a4d3df087be4a2
KamelZyadeh <kamelzyadeh94@gmail.com>
2020-04-13 18:52:01 +0300

    adding a human

---
+++ Human.java
@@ -0,0 +1,32 @@
+package kamel;
+
+public class Human{
+
+    public int height;
+    public String name;
+    public String eyeColor;
+    public int hairLength;
+    public boolean fat;
+
+    public boolean isFat() {
+        return fat;
+    }
+
+    public int getHairLength() {
+        return 100;
+    }
+
+    public int getHeight() {
+        return 175;
+    }
+
+    public String getEyeColor() {
+        return "black";
+    }
+
+    public String getName() {
+        return "Ali";
+
+    }
+
+}
